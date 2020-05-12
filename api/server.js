61d4bebafd8f0d79046336906d2b3eccd0ff83e8
Daniel Lim <danheelim@gmail.com>
2020-05-11 23:42:58 -0700

    nodemon and local server is up

--- api/server.js
+++ api/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 
-const configureMiddleWare = require('./middleware.js');
+// const configureMiddleWare = require('./middleware.js');
 
 const server = express();
-configureMiddleWare(server);
+// configureMiddleWare(server);
 
 server.get('/', (req, res) => {
     res.send('testing mock server, main splash page goes here!');
 });
+
+// server.listen(5000, () => {
+//     console.log('Server running on local host')
+// })
 
 module.exports = server;
