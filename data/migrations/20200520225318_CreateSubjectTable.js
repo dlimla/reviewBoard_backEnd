d663c89bca38f744e3f4786c2f14c3c43bc12ee4
Daniel Lim <danheelim@gmail.com>
2020-05-20 22:58:00 -0700

    adds subject categorytable.

---
+++ data/migrations/20200520225318_CreateSubjectTable.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('category', tbl => {
+    tbl.increments();
+    tbl.string('category_name')
+    tbl.integer('object_id')
+        .unique()
+        .unsigned()
+        .reference('id')
+        .inTable('object')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('category')
+};
