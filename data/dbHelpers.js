1d7012d9ce35f8aa2f3d8355fbbc44d7e0bebdcf
Daniel Lim <danheelim@gmail.com>
2020-06-09 20:36:20 -0700

    added helpers and first async function to get all objects

---
+++ data/dbHelpers.js
@@ -0,0 +1,23 @@
+const knex = require('knex')
+
+const dbConfig = require('../knexfile.js');
+
+const db = knex(dbConfig.development);
+
+
+module.exports = {
+    getAllReviews
+}
+
+async function getAllReviews() {
+    return db('object')
+        .innerJoin('review', 'object_review', 'object.id')
+        .select(
+            'object_name',
+            'object_manufac',
+            'object_price',
+            'object_country_origin',
+            'review.review as object review',
+            'review_created_data as created on'
+        )
+}
