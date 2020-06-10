46a996ee5620e97803eab28111a241c0b897126c
Daniel Lim <danheelim@gmail.com>
2020-06-10 00:30:08 -0700

    added get single object with review endpoint

--- data/dbHelpers.js
+++ data/dbHelpers.js
@@ -6,7 +6,8 @@ const db = knex(dbConfig.development);
 
 
 module.exports = {
-    getAllReviews
+    getAllReviews,
+    getOneObject
 }
 
 async function getAllReviews() {
@@ -20,4 +21,18 @@ async function getAllReviews() {
             'review.review as object review',
             'review_created_data as created on'
         )
+}
+
+async function getOneObject(id) {
+    return db('object')
+        .innerJoin('review', 'object_review', 'object.id')
+        .select(
+            'object_name',
+            'object_manufac',
+            'object_price',
+            'object_country_origin',
+            'review.review as object review',
+            'review_created_data as created on'
+        )
+        .where({ id })
 }
